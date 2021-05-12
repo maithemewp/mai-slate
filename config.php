252ec05c681c8e51c6edf49aaf620b0dd7b9b891
Mike Hemberger <mike@thestizmedia.com>
2021-05-12 10:08:00 -0400

    Get it started

---
+++ config.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Mai Engine default settings config.
+ * This config sets default settings.
+ * Once settings are saved in the Customizer the saved values takeover.
+ *
+ * Use https://bizbudding.com/mai-theme/plugins/mai-config-generator to generate a custom config for your site defaults.
+ * or see /mai-engine/config/_default.php for all config options.
+ *
+ * @package   BizBudding\MaiEngine
+ * @link      https://bizbudding.com
+ * @author    BizBudding
+ * @copyright Copyright © 2020 BizBudding
+ * @license   GPL-2.0-or-later
+ */
+
+return [
+	// 'global-styles' => [
+		// 'colors' => [
+		// 	'black'      => '#000000',
+		// 	'white'      => '#ffffff',
+		// 	'header'     => '#ffffff', // Site header background.
+		// 	'background' => '#ffffff', // Body background.
+		// 	'alt'        => '#f8f9fa', // Background alt.
+		// 	'body'       => '#6c747d', // Body text color.
+		// 	'heading'    => '#343a40', // Heading text color.
+		// 	'link'       => '#007bff', // Link color.
+		// 	'primary'    => '#007bff', // Button primary background color.
+		// 	'secondary'  => '#6c747d', // Button secondary background color.
+		// ],
+		// 'custom-colors' => [
+			// [
+			// 	'color' => '#bcda83', // var(--color-custom-1).
+			// ],
+		// ],
+	// 	'fonts'          => [
+	// 		'body'    => 'sans-serif:400',
+	// 		'heading' => 'sans-serif:600',
+	// 	],
+	// ],
+	// 'theme-support' => [
+	// 	'add' => [
+	// 		'sticky-header',
+	// 		'transparent-header',
+	// 	],
+	// ],
+	// 'image-sizes' => [
+	// 	'add' => [
+	// 		'landscape' => '4:3',
+	// 		'portrait'  => '3:4',
+	// 		'square'    => '1:1',
+	// 	],
+	// ],
+	// 'template-parts' => [
+	// 	'custom-location' => [
+	// 		'hook'       => 'genesis_after_header',
+	// 		'priority'   => 10,
+	// 		'menu_order' => 80,
+	// 		'before'     => '<div class="custom-location template-part">',
+	// 		'after'      => '</div>',
+	// 		'condition'  => function() {
+	// 			return is_front_page() || is_category();
+	// 		},
+	// 	],
+	// ],
+];
---
+++ vendor/composer/installed.php
@@ -0,0 +1,52 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '3094f1ae7d8fe868ff9bac18069ececba84abf1f',
+    'name' => 'maithemewp/mai-inspire',
+  ),
+  'versions' => 
+  array (
+    'afragen/wp-dependency-installer' => 
+    array (
+      'pretty_version' => '3.1.0',
+      'version' => '3.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '65570a1eb966bb9adfc9d9e521357da080fbdc5a',
+    ),
+    'collizo4sky/persist-admin-notices-dismissal' => 
+    array (
+      'pretty_version' => '1.4.4',
+      'version' => '1.4.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '900739eb6b0ec0210465f5983a6d4e0e420289e4',
+    ),
+    'maithemewp/mai-inspire' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3094f1ae7d8fe868ff9bac18069ececba84abf1f',
+    ),
+    'maithemewp/mai-installer' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => 'ab01c60c49a9f20a50d59adf329715145cb311ea',
+    ),
+  ),
+);
