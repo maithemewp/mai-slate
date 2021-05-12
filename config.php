dcc9ae13cec63ceacccee0e81e74f791828abefa
Mike Hemberger <mike@thestizmedia.com>
2021-05-12 10:18:08 -0400

    default config examples

--- config.php
+++ config.php
@@ -15,35 +15,38 @@
  */
 
 return [
+	// // Default global styles, colors, and fonts.
 	// 'global-styles' => [
-		// 'colors' => [
-		// 	'black'      => '#000000',
-		// 	'white'      => '#ffffff',
-		// 	'header'     => '#ffffff', // Site header background.
-		// 	'background' => '#ffffff', // Body background.
-		// 	'alt'        => '#f8f9fa', // Background alt.
-		// 	'body'       => '#6c747d', // Body text color.
-		// 	'heading'    => '#343a40', // Heading text color.
-		// 	'link'       => '#007bff', // Link color.
-		// 	'primary'    => '#007bff', // Button primary background color.
-		// 	'secondary'  => '#6c747d', // Button secondary background color.
-		// ],
-		// 'custom-colors' => [
-			// [
-			// 	'color' => '#bcda83', // var(--color-custom-1).
-			// ],
-		// ],
-	// 	'fonts'          => [
-	// 		'body'    => 'sans-serif:400',
-	// 		'heading' => 'sans-serif:600',
+	// 	'colors' => [
+	// 		'black'      => '#000000',
+	// 		'white'      => '#ffffff',
+	// 		'header'     => '#ffffff', // Site header background.
+	// 		'background' => '#ffffff', // Body background.
+	// 		'alt'        => '#f8f9fa', // Background alt.
+	// 		'body'       => '#6c747d', // Body text color.
+	// 		'heading'    => '#343a40', // Heading text color.
+	// 		'link'       => '#007bff', // Link color.
+	// 		'primary'    => '#007bff', // Button primary background color.
+	// 		'secondary'  => '#6c747d', // Button secondary background color.
+	// 	],
+	// 	'custom-colors' => [
+	// 		[
+	// 			'color' => '#bcda83', // var(--color-custom-1).
+	// 		],
+	// 	],
+	// 	'fonts'  => [
+	// 		'body'    => 'Roboto:300,500', // 300 is regular and 500 is bold.
+	// 		'heading' => 'Playfair Display:400',
 	// 	],
 	// ],
+	// // Set default theme support settings.
 	// 'theme-support' => [
 	// 	'add' => [
 	// 		'sticky-header',
 	// 		'transparent-header',
 	// 	],
 	// ],
+	// // Enable image orientations and set aspect ratio.
 	// 'image-sizes' => [
 	// 	'add' => [
 	// 		'landscape' => '4:3',
@@ -51,6 +54,7 @@ return [
 	// 		'square'    => '1:1',
 	// 	],
 	// ],
+	// // Custom template parts.
 	// 'template-parts' => [
 	// 	'custom-location' => [
 	// 		'hook'       => 'genesis_after_header',
@@ -58,6 +62,7 @@ return [
 	// 		'menu_order' => 80,
 	// 		'before'     => '<div class="custom-location template-part">',
 	// 		'after'      => '</div>',
+	// 		// Conditionally display this template part.
 	// 		'condition'  => function() {
 	// 			return is_front_page() || is_category();
 	// 		},
